48edca20999d5f84351218f4d2e658349f9f5ecc
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-21 23:15:47 -0500

    Indez DetallePlato

---
+++ Pages/DetallesPlatos/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestoStockDB1.Pages.DetallesPlatos
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
