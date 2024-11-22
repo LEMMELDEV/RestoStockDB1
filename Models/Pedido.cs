4215210066c164e5406df880c6b400ce41aeefc5
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-21 22:15:41 -0500

    Model Pedido

---
+++ Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿namespace RestoStockDB1.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; } // Llave primaria
+
+        // Llave foránea
+        public int ProovedorId { get; set; }
+        public Proovedor Proovedor { get; set; }
+
+        public DateTime FechaPedido { get; set; }
+        public decimal Total { get; set; }
+    }
+}
