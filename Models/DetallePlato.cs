49553d166037c89fe125b719bbc3611ebd16afcd
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-21 22:08:25 -0500

    Model DetallePlato

---
+++ Models/DetallePlato.cs
@@ -0,0 +1,16 @@
+﻿namespace RestoStockDB1.Models
+{
+    public class DetallePlato
+    {
+        public int DetallePlatoId { get; set; } // Llave primaria
+
+        // Llaves foráneas
+        public int PlatoId { get; set; }
+        public Plato Plato { get; set; }
+
+        public int ingredienteId { get; set; }
+        public Ingrediente Ingrediente { get; set; }
+
+        public decimal Cantidad { get; set; }
+    }
+}
