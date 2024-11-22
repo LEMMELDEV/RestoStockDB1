c0908a960e1c0d33c608113d14d1414c7c8030e4
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-21 22:15:01 -0500

    Model Ingrediente

---
+++ Models/Ingrediente.cs
@@ -0,0 +1,14 @@
+﻿namespace RestoStockDB1.Models
+{
+    public class Ingrediente
+    {
+        public int IngredienteId { get; set; } // Llave primaria
+        public string Nombre { get; set; }
+        public decimal CantidadDisponible { get; set; }
+        public string UnidadMedida { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        // Relación muchos-a-muchos con Plato a través de DetallePlato
+        public ICollection<DetallePlato> DetallesPlatos { get; set; }
+    }
+}
