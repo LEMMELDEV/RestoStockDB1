aecd634550b198a23edc10e335c8711d74339773
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-21 22:16:53 -0500

    Model Proovedor

---
+++ Models/Proovedor.cs
@@ -0,0 +1,15 @@
+﻿namespace RestoStockDB1.Models
+{
+    public class Proovedor
+    {
+        public int ProovedorId { get; set; } // Llave primaria
+        public string NombreEmpresa { get; set; }
+        public string Contacto { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+
+        // Relación uno-a-muchos con Pedido
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
