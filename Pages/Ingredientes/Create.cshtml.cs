f358c0005f919edb49ff4077d32d368a75a85f8e
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2024-11-22 18:59:32 -0500

    Ingre y pedido modelo

---
+++ Pages/Ingredientes/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestoStockDB1.Data; 
+using RestoStockDB1.Models;
+
+namespace RestoStockDB1.Pages.Ingredientes
+{
+    public class CreateModel : PageModel
+    {
+       
+    }
+}
