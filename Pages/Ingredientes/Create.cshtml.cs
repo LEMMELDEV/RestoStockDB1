52401d5843728a9b525d4d397f6ecdf49bc9121c
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2024-11-22 19:21:45 -0500

    create ingredientes

--- Pages/Ingredientes/Create.cshtml.cs
+++ Pages/Ingredientes/Create.cshtml.cs
@@ -7,6 +7,32 @@ namespace RestoStockDB1.Pages.Ingredientes
 {
     public class CreateModel : PageModel
     {
-       
+        private readonly RestoStockContext _context;
+
+        public CreateModel(RestoStockContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Ingrediente Ingrediente { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Ingredientes == null || Ingrediente == null)
+            {
+                return Page();
+            }
+
+            _context.Ingredientes.Add(Ingrediente);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
     }
 }
