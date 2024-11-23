9433047671178baeda4bdbd05a9c4a282e48836e
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2024-11-22 19:53:14 -0500

    Edit ingredientes

--- Pages/Ingredientes/Edit.cshtml.cs
+++ Pages/Ingredientes/Edit.cshtml.cs
@@ -1,12 +1,75 @@
+using RestoStockDB1.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestoStockDB1.Models;
 
 namespace RestoStockDB1.Pages.Ingredientes
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly RestoStockContext _context;
+
+        public EditModel(RestoStockContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Ingrediente Ingrediente { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            if (id == null || _context.Ingredientes == null)
+            {
+                return NotFound();
+            }
+
+            var ingrediente = await _context.Ingredientes
+                .Include(i => i.DetallesPlatos) // Incluye las relaciones con DetallePlato
+                .FirstOrDefaultAsync(m => m.IngredienteId == id);
+
+            if (ingrediente == null)
+            {
+                return NotFound();
+            }
+
+            Ingrediente = ingrediente;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Ingrediente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IngredienteExists(Ingrediente.IngredienteId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool IngredienteExists(int id)
+        {
+            return (_context.Ingredientes?.Any(e => e.IngredienteId == id)).GetValueOrDefault();
         }
     }
 }
